52f578a6f94a7c23cb97d140a117ca4ea6aca859
Martin Chaov <martin.c@sbtech.com>
2020-08-12 19:20:25 +0300

    added found router and react component to render <App/>

--- packages/package/src/index.tsx
+++ packages/package/src/index.tsx
@@ -1,14 +1,38 @@
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
 import { Pages } from "./pages";
 
 import "./index.less";
 import "highlight.js/styles/github.css";
 import "../../../node_modules/github-markdown-css/github-markdown.css"
 
+
+class App extends React.Component<{}, {}>{
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <>
+            <h1>
+                Top
+            </h1>
+            
+            <div
+                className="markdown-body"
+                dangerouslySetInnerHTML={{
+                    __html: Pages.test
+                }}>
+            </div>
+        </>
+    }
+}
+
 export function init(domId: string) {
-    document.body.innerHTML = `
-    <div class="markdown-body">
-    ${Pages.test}
-    </div>
-    `;
-    console["log"]("init", domId);
+    ReactDOM.render(
+        <App />,
+        document.getElementById(domId)
+    )
 }
