6ebd4ebb786f01ea9b7116e983393b0743326b97
Martin Chaov <martin.c@sbtech.com>
2020-08-12 11:42:58 +0300

    test react setup and some additional files

--- packages/package/src/index.tsx
+++ packages/package/src/index.tsx
@@ -1,3 +1,42 @@
+import React from "react";
+import { Component } from "react";
+import { render } from "react-dom";
+import { observable, action } from "mobx";
+import { observer } from "mobx-react";
+
 import "./index.less";
 
-export function init() { }
+class TestState {
+    private timeout: any;
+    @observable count: number = 0;
+
+    constructor() {
+        this.timeout = this.start();
+    }
+
+    start = () => {
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(this.start, 1000);
+    }
+
+    @action inc() {
+        this.count += 1;
+    }
+}
+
+@observer
+class Test extends Component<{ state: TestState }, TestState> {
+    constructor(props) {
+        super(props);
+        this.state = this.props.state
+    }
+    render() {
+        return <div>test</div>
+    }
+}
+
+export function init(domId: string) {
+    render(<Test state={new TestState()} />, document.getElementById(domId));
+}
+
+window["init"] = init;
