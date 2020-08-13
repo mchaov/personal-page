1fa9677a1319186ab96be1c41497b2845fc033b6
Martin Chaov <martin.c@sbtech.com>
2020-08-13 20:31:32 +0300

    cleanup

--- packages/package/src/articles/index.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-import Test from "./test.md";
-
-export const Articles = {
-    test: Test
-}
--- packages/package/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { init } from "./index";
-
-describe("Test of the setup", () => {
-    it("tests work", () => {
-        expect(typeof init).toBe("function");
-    })
-})
